587f09a32c7686eb5535073e46430351a1c8f754
MATHEUS FERNANDES DE FARIA <51159288844@admin.br>
2019-04-23 16:29:26 -0300

    Now w/ login

---
+++ login.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <!--<meta charset="utf-8">-->
+	<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link href="css/agency.min.css" rel="stylesheet">
+    <title>FECAP - Login de Eventos</title>
+
+    <style type="text/css">
+    	label{
+    		/*font-family: verdana;*/
+    		font-size: 120%;
+    		margin-left: 50px;
+    		color:#033800;
+
+
+    	}
+
+    </style>
+
+  </head>
+
+  <body style="background-color:white"></br>
+   	<div class="row justify-content-md-center justify-content-sm-center"style="background-color: white">
+   		<div class="login text-center col-lg-5 col-md-10 col-sm-1 ">
+
+
+   		<form style="background-color:white"class="col-sm-2 col-md-12 ">
+
+   			  	<div style="margin-top:20px; margin-right: 30px; background-color: white">
+					<img src="https://www.fecap.br/trabalhe-conosco/assets/img/logo-fecap.png" class="img-fluid" style="max-width: 120px"><br><br>
+					<h3 class="text-muted" style="font-family: verdana;">Login FECAP</h3>
+				</div></br>
+
+				<div class="form-group col-sm-1 col-md-10">
+					<label for="nome"style="margin-left: -60%">Usuário </label><br/>
+					<input type="text" name="nome" class="form-control" style="margin-left: 30px;width:90%; font-size: 120%;height: 45px;background-color: #f7f9f7"placeholder="Usuário" required maxlength="80"></div>
+
+				<div class="form-group col-sm-1 col-md-10">
+					<label for="nome"style="margin-left: -65%">Senha </label><br/>
+					<input type="password" name="nome" class="form-control" style="margin-left: 30px;width:90%; font-size: 120%;height: 45px;background-color: #f7f9f7"placeholder="Senha" required maxlength="100">
+					<label style="font-family: verdana; font-size:10px;margin-left: -5px">* Preencha com o usuário e senha da intranet</label></div> <br/>
+
+				<div class="form-group col-sm-1 col-md-10 text-center">
+					<button  class="btn btn-primary" onclick="window.location='index.php';" style="background-color: #1E6631;border-color: transparent">Entrar</button>
+				</div>
+
+
+
+  		</div></div>
+</form>
+
+
+
+			<br>
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
+				
